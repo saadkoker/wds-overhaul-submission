69f00969860395601d0512273f126d26da7364dc
saadkoker <60635704+saadkoker@users.noreply.github.com>
2023-12-02 17:36:54 -0500

    This took too long

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+class Note {
+    constructor(title) {
+        this.title = title;
+        this.content = '';
+        this.button; // Reference to the associated button
+    }
+}
+
+let notes = [];
+let buttonIDs = [];
+let currentNote;
+let currentID = 0;
+
 document.addEventListener("DOMContentLoaded", attachListeners);
 document.addEventListener("DOMContentLoaded", checkPage);
 
@@ -12,14 +25,73 @@ function attachListeners() {
     if (addButton) {
         addButton.addEventListener('click', addFile);
     }
+
+    var textarea = document.querySelector('.writing');
+
+    // Check if the textarea element exists
+    if (textarea) {
+        // Add an 'input' event listener to the textarea
+        textarea.addEventListener('input', function() {
+            // Get the content of the textarea when it's updated
+            if (currentNote) {
+                currentNote.content = textarea.value;
+            }
+        });
+    }
+    document.addEventListener('click', function(event) {
+        // Check if the clicked element has the class "note"
+        if (event.target.classList.contains('note')) {
+            // Update the 'buttonCurrentlySelected' variable
+            buttonCurrentlySelected = event.target;
+
+            // Retrieve the note associated with the clicked button
+            currentNote = notes[parseInt(buttonCurrentlySelected.getAttribute("id"))];
+            console.log('Content of the clicked note:', currentNote.content);
+
+
+            // Update the textarea content with the note content
+            let writingTextarea = document.querySelector('.writing');
+            writingTextarea.value = currentNote.content;
+        }
+    });
+}
+function editTextArea(noteToReplace) {
+    // Save the content of the current textarea to the current note
+    if (currentNote) {
+        currentNote.content = document.querySelector('.writing').value;
+    }
+
+    // Set the current note to the one being edited
+    currentNote = noteToReplace;
+
+    // Update the textarea with the content of the note being edited
+    let writingTextarea = document.querySelector('.writing');
+    writingTextarea.value = currentNote.content;
 }
 
 function addFile() {
     let filebar = document.querySelector('.files');
-    let newFileButton = document.createElement('button');
-    newFileButton.textContent = 'My New File';
-    newFileButton.addEventListener('click', () => console.log("Go to new note"));
-    filebar.appendChild(newFileButton);
+    let id = currentID.toString();
+
+    let fileName = prompt('Enter a unique name for your new file:');
+    if (!fileName) {
+        // If the user cancels the prompt, return early
+        return;
+    }
+    let newNote = new Note(fileName);
+    newNote.button = document.createElement('button');
+    notes.push(newNote);
+
+    newNote.button.setAttribute("id", id);
+    newNote.button.setAttribute("class", "note");
+    newNote.button.textContent = fileName;
+    newNote.button.addEventListener('click', () => editTextArea(newNote));
+    filebar.appendChild(newNote.button);
+    currentID = currentID + 1;
+    // Clear the content of the textarea and set the current note to the new note
+    let writingTextarea = document.querySelector('.writing');
+    writingTextarea.value = '';  // Clear the textarea
+    currentNote = newNote;
 }
 
 function checkPage() {
